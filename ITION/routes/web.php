f230bdcfed10fb273c931484bdcca8e6e8e73ab6
Aldo Faiz Winarno <71934691+aldofaiz@users.noreply.github.com>
2021-05-08 19:57:34 +0700

    use controller

--- ITION/routes/web.php
+++ ITION/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\lombaController;
-use App\Http\Controllers\detailLombaController;
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\LombaController;
+use App\Http\Controllers\GaleriController;
+use App\Http\Controllers\TestimoniController;
 use Illuminate\Support\Facades\DB;
 /*
 |--------------------------------------------------------------------------
@@ -15,46 +17,15 @@ use Illuminate\Support\Facades\DB;
 |
 */
 
-Route::get('/', function () {
-    $id_lomba_random = rand(1,2);
-    $rekom = DB::select("SELECT lomba.id_lomba,lomba.poster,lomba.judul,kategori.nama_kategori 
-    FROM lomba,kategori 
-    WHERE lomba.id_lomba = $id_lomba_random AND lomba.id_kategori = kategori.id_kategori");
+Route::get('/', [BerandaController::class, 'index']);
 
-    $galeri = DB::select("SELECT galeri.id_galeri,galeri.foto,kategori.nama_kategori 
-    FROM galeri,lomba,kategori 
-    WHERE galeri.id_lomba = lomba.id_lomba AND lomba.id_kategori = kategori.id_kategori");
-    
-    return view('beranda.beranda',['rekom' => $rekom, 'galeri' => $galeri]);
-});
+Route::get('/lomba',[LombaController::class, 'index']);
 
-Route::get('/lomba',function(){
-    $data = DB::select("SELECT lomba.id_lomba,lomba.poster,lomba.judul,kategori.nama_kategori 
-    FROM lomba,kategori 
-    WHERE lomba.id_kategori = kategori.id_kategori");
-        return view('lomba.lomba',['data' => $data]);
-});
-
-Route::get('/lomba/{id_lomba?}',function($id_lomba){
-    $data = DB::select("SELECT lomba.id_lomba,lomba.poster,lomba.judul,kategori.nama_kategori,lomba.biaya,penyelenggara_lomba.kontak
-    FROM lomba,kategori,penyelenggara_lomba 
-    WHERE lomba.id_lomba = $id_lomba AND lomba.id_kategori = kategori.id_kategori AND lomba.id_penyelenggara = penyelenggara_lomba.id_penyelenggara");
-    if(empty($data)){
-        return abort(404);
-    }
-    else{
-        return view('lomba.detail_lomba',['data' => $data]);
-    }
-});
+Route::get('/lomba/{id_lomba?}',[LombaController::class, 'indexDetail']);
 
 Route::view('/testimoni', 'testimoni.testimoni');
 
-Route::get('/galeri',function(){
-    $data = DB::select("SELECT galeri.id_galeri,galeri.foto,kategori.nama_kategori 
-    FROM galeri,lomba,kategori 
-    WHERE galeri.id_lomba = lomba.id_lomba AND lomba.id_kategori = kategori.id_kategori");
-        return view('galeri.galeri',['data' => $data]);
-});
+Route::get('/galeri',[GaleriController::class, 'index']);
 
 Route::view('/tentang', 'tentang.tentang');
 
