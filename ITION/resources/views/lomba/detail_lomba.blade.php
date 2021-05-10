3ba725a5425bc0434ab1f33f1baac892d85c883b
nikefebr <74341372+nikefebr@users.noreply.github.com>
2021-05-10 13:46:28 +0700

    Detail Lomba

--- ITION/resources/views/lomba/detail_lomba.blade.php
+++ ITION/resources/views/lomba/detail_lomba.blade.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" {{ asset('css/style.css') }}>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Detail Lomba</title>
 </head>
-<body>
+<body style="background-color: #F4F4F4;">
     @include('navbar')
-    <h1>Ini Halaman Detail Lomba</h1>
+    
     <tabel border="1">
     @foreach ($data as $item)
     <tr>
