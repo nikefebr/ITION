c6512044d5ac3f9f597c38a4c830e64885344670
nikefebr <74341372+nikefebr@users.noreply.github.com>
2021-05-10 12:43:51 +0700

    Lomba

--- ITION/resources/views/lomba/lomba.blade.php
+++ ITION/resources/views/lomba/lomba.blade.php
@@ -1,16 +1,117 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" {{ asset('css/style.css') }}>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Lomba</title>
 </head>
-<body>
+
+<body style="background-color: #F4F4F4;">
     @include('navbar')
-    <h1>Ini Halaman Lomba</h1>
+    
+    <div class="bg-blue-gradient">
+    <div class="p-3"></div>
+        <div class="container p-5">
+            <div class="row">
+                <img class="img-fluid mx-auto" src="{{asset('image/lomba.png')}}" style="width: 800px; height: 450px">
+            </div>
+
+            <div class="p-5"></div>
+            <div class="p-1"></div>
+
+            <div class="row text-white">
+                <h4 class="fw-bold text-center">Keuntungan sering mengikuti lomba</h4>
+                
+                <div class="p-5"></div>
+
+                <div class="row fw-bold text-center">
+                    <div class="col-md-3 col-sm-6">
+                        <p>Langkah untuk menjadi<br>
+                            Mawapres</p>
+                        <img src="{{asset('logo/mawapres-white.png')}}">
+                    </div>
+
+                    <div class="col-md-3 col-sm-6">
+                        <p>Personal Branding</p>
+                        <img src="{{asset('logo/branding-white.png')}}">
+                    </div>
+
+                    <div class="col-md-3 col-sm-6">
+                        <p>Membuat<br>
+                            CV lebih menarik</p>
+                        <img src="{{asset('logo/cv-white.png')}}">
+                    </div>
+
+                    <div class="col-md-3 col-sm-6">
+                        <p>Mengasah skillmu</p>
+                        <img src="{{asset('logo/skill-white.png')}}">
+                    </div>
+
+                    <div class="p-1"></div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        
+        <div class="p-5"></div>
+        <div class="mt-1"></div>
+
+        <h5 class="fw-bold text-center">Ikuti Lomba</h5>
+
+        <div class="p-4"></div>
+
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+
+                <div class="p-3"></div>
+
+                <div class="card" style="width: 25rem;">
+                    <div class="row">
+                        <img src="" class="card-img-top" alt="">
+                            <div class="card-img-overlay">
+                                <p class="card-text">Publish </p>
+                            </div>
+                    </div>
+
+                    <div class="p-3"></div>
+
+                    <div class="row">
+                        <div class="col-md-7 col-sm-7">
+                            <div class="card-body">
+                                <h5 class="card-text fw-bold text-blue">Judul</h5>
+                                <h6 class="card-text fw-light text-blue">Kategori</h6>
+                                
+                                <p class="card-text text-grey mt-4">
+                                    Deskripsi
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="col-md-5 col-sm-5 mt-0 pt-0">
+                            <div class="p-2"></div>
+
+                            <div class="card-body bg-blue-2 text-center">
+                                <h6 class="card-text fw-bold text-blue text-white">DEADLINE</h6>
+                                <h6 class="card-text fw-bold text-blue text-white">PENDAFTARAN</h6>
+                                
+                                <p class="card-text mt-4">
+                                    
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row ms-0" style="width: 25rem;">
+                        <button class="button-katalog fw-bolder text-blue-2">Daftar Lomba Ini</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="p-5"></div>
+
     <div>
     <tabel border="3">
         <tr>
