ca8be81816a709f1d3f66f95b493bbbc00d5bbe5
nikefebr <74341372+nikefebr@users.noreply.github.com>
2021-05-11 19:17:37 +0700

    Update testimoni

--- ITION/resources/views/testimoni/testimoni.blade.php
+++ ITION/resources/views/testimoni/testimoni.blade.php
@@ -1,16 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" {{ asset('css/style.css') }}>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Testimoni</title>
 </head>
-<body>
+<body style="background-color: #F4F4F4;">
     @include('navbar')
-    <h1>Ini Halaman Testimoni</h1>
+
+    <div class="p-3"></div>
+    
+    <div class="container p-5">
+        <div class="row">
+            <img class="img-fluid mx-auto" style="width: 800px; height: 450px">
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="pt-5"></div>
+        <div class="pt-5"></div>
+        <div class="pt-5"></div>
+
+        <div class="row">
+            <h2 class="fw-bold">Gimana sih rasanya ikut ...?</h2>
+        </div>
+
+        <div class="row">
+            <div class="row">
+                <div class="p-3"></div>
+
+                <div class="col-md-3 col-sm-6 float-start">
+                    <div class="card" style="background-color:#F4F4F4; width: 18rem; border:none;">
+                        <img src="" class="card-img-top" alt="" width="200" height="200">
+                            <div class="card-body">
+                                <p class="card-text fw-normal"></p>
+                                <p class="card-text fw-bold"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <tabel class="table table-bordered">
     <thead>
         <tr>
