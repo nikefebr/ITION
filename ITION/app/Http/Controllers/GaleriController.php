77851fe4610daf35061efb39b2c5766a7c7b8ed9
Aldo Faiz Winarno <71934691+aldofaiz@users.noreply.github.com>
2021-05-12 19:48:13 +0700

    update galeri

--- ITION/app/Http/Controllers/GaleriController.php
+++ ITION/app/Http/Controllers/GaleriController.php
@@ -10,10 +10,10 @@ class GaleriController extends Controller
     //
     public function index()
     {
-        $data = DB::select("SELECT galeri.id_galeri,galeri.foto,kategori.nama_kategori 
+        $data = DB::select("SELECT galeri.id_galeri,galeri.foto,galeri.deskripsi,lomba.tahun,kategori.nama_kategori 
         FROM galeri,lomba,kategori 
         WHERE galeri.id_lomba = lomba.id_lomba 
-        AND lomba.id_kategori = kategori.id_kategori ");
+        AND lomba.id_kategori = kategori.id_kategori");
         // AND kategori.nama_kategori = 'Hackathon'
         return view('galeri.galeri',['data' => $data]);
     }
