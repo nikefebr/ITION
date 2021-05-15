500fc9e105c95e9736bf6e69868678c814dc57aa
nikefebr <74341372+nikefebr@users.noreply.github.com>
2021-05-15 10:11:31 +0700

    update detil lomba dan css nya

--- ITION/resources/views/lomba/detail_lomba.blade.php
+++ ITION/resources/views/lomba/detail_lomba.blade.php
@@ -5,6 +5,80 @@
 </head>
 <body style="background-color: #F4F4F4;">
     @include('navbar')
+
+    <div class="p-4"></div>
+    
+    <div class="container">
+        <div class="row">
+        <img class="img-fluid mx-auto" src="{{asset('image/detail_lomba.png')}}">
+        </div>
+    </div>
+
+    <div class="p-4"></div>
+
+    <div class="container px-5 px-5">
+        <div class="row">
+            <div class="col-md-4 col-sm-5">
+                <h1 class="fw-bold">Deskripsi</h1>
+                <img class="img-fluid mx-auto">Poster
+                <h5 class="fw-normal">Deskripsi</h5>
+            </div>
+
+            <div class="col-md-4 col-sm-5"></div>
+
+            <div class="col-md-4 col-sm-5 px-5">
+                <div class="card px-3 pt-3" style="width: 23rem;">
+                    <h5>Tunggu apa lagi ayo daftar Lomba Hackathon sekarang :D <br><br>
+                    Informasi CP : <br>
+                    WA. 087701437591 (Anon) <br>
+                    </h5>
+                    <a href="">Klik disini untuk menghubungi CP !</a>
+
+                    <div class="p-4"></div>
+
+                    <div class="px-5">
+                        <button class="button-daftar-lomba fw-bolder">Daftar Sekarang</button>
+                    </div>
+                </div>
+
+                <div class="p-4"></div>
+
+                <div class="px-5">
+                    <button class="white-button-daftar fw-bolder">Buku Panduan</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-4"></div>
+
+        <div class="row">
+            <h1 class="fw-bold">Syarat Lomba</h1>
+            <h5 class="fw-normal">Deskripsi</h5>
+        </div>
+
+        <div class="p-4"></div>
+
+        <div class="row">
+            <h1 class="fw-bold">Timeline</h1>
+            <h5 class="fw-normal">Deskripsi</h5>
+        </div>
+
+        <div class="p-4"></div>
+
+        <div class="row">
+            <h1 class="fw-bold">Biaya Pendaftaran</h1>
+            <h5 class="fw-normal">Deskripsi</h5>
+        </div>
+
+        <div class="p-4"></div>
+
+        <div class="row">
+            <h1 class="fw-bold">Juara + Hadiah</h1>
+            <h5 class="fw-normal">Deskripsi</h5>
+        </div>
+    </div>
+
+    <div class="p-4"></div>
     
     <tabel border="1">
     @foreach ($data as $item)
@@ -18,6 +92,9 @@
     </tr>
     @endforeach
     </tabel>
+
+    <div class="p-4"></div>
+    
     @include('footer')
 </body>
 </html>
