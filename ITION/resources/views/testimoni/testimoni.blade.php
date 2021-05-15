1322ef249390660b138ff9221c81c5d2544c93fb
MahdiyaAqila <mahdiyaqila@gmail.com>
2021-05-15 10:57:55 +0800

    Update testimoni.blade.php

--- ITION/resources/views/testimoni/testimoni.blade.php
+++ ITION/resources/views/testimoni/testimoni.blade.php
@@ -20,7 +20,43 @@
         <div class="pt-5"></div>
 
         <div class="row ms-5">
-            <h2 class="fw-bold">Gimana sih rasanya ikut ...?</h2>
+            <h2 class="fw-bold mb-5">Gimana sih rasanya ikut ...?</h2>
+             <div class="col">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                           <img src="/image/reviewer/Muhammad zaid.jpg" alt="" class=" img-fluid rounded-circle p-3" style="width: 120px; height: 120px;display: block; margin-left: auto; margin-right: auto;">
+                            <h5 class="px-3 pt-3 text-bold">Mahdiya Aqila</h5>
+                            <p class="px-3 text-bold" style="font-size: 10pt; opacity: 50%;">KMIPN Hackathon </p>
+                            <p class="px-3" style="font-size: 10pt;">
+                                “Selama saya mengikuti KMIPN, banyak hal yang bisa saya dapatkan. Mulai dari pengalaman, relasi, dan kesempatan - kesempatan emas lainnya. Skill saya jugaberkembang setelah mengikuti lomba tersebut karena telah direview oleh juri yang profesional. “ - Nanang,2018.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                 <div class="col">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                           <img src="/image/reviewer/Muhammad zaid.jpg" alt="" class=" img-fluid rounded-circle p-3" style="width: 120px; height: 120px;display: block; margin-left: auto; margin-right: auto;">
+                            <h5 class="px-3 pt-3 text-bold">Mahdiya Aqila</h5>
+                            <p class="px-3 text-bold" style="font-size: 10pt; opacity: 50%;">KMIPN Hackathon </p>
+                            <p class="px-3" style="font-size: 10pt;">
+                                “Selama saya mengikuti KMIPN, banyak hal yang bisa saya dapatkan. Mulai dari pengalaman, relasi, dan kesempatan - kesempatan emas lainnya. Skill saya jugaberkembang setelah mengikuti lomba tersebut karena telah direview oleh juri yang profesional. “ - Nanang,2018.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                 <div class="col">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                           <img src="/image/reviewer/Muhammad zaid.jpg" alt="" class=" img-fluid rounded-circle p-3" style="width: 120px; height: 120px;display: block; margin-left: auto; margin-right: auto;">
+                            <h5 class="px-3 pt-3 text-bold">Mahdiya Aqila</h5>
+                            <p class="px-3 text-bold" style="font-size: 10pt; opacity: 50%;">KMIPN Hackathon </p>
+                            <p class="px-3" style="font-size: 10pt;">
+                                “Selama saya mengikuti KMIPN, banyak hal yang bisa saya dapatkan. Mulai dari pengalaman, relasi, dan kesempatan - kesempatan emas lainnya. Skill saya jugaberkembang setelah mengikuti lomba tersebut karena telah direview oleh juri yang profesional. “ - Nanang,2018.
+                            </p>
+                        </div>
+                    </div>
+                </div>
         </div>
         
         <div class="p-5"></div>
@@ -50,7 +86,43 @@
         <div class="p-5"></div>
        
         <div class="row ms-5">
-            <h2 class="fw-bold">Gimana sih rasanya ikut ...?</h2>
+            <h2 class="fw-bold mb-5">Gimana sih rasanya ikut ...?</h2>
+             <div class="col">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                           <img src="/image/reviewer/Muhammad zaid.jpg" alt="" class=" img-fluid rounded-circle p-3" style="width: 120px; height: 120px;display: block; margin-left: auto; margin-right: auto;">
+                            <h5 class="px-3 pt-3 text-bold">Mahdiya Aqila</h5>
+                            <p class="px-3 text-bold" style="font-size: 10pt; opacity: 50%;">KMIPN Hackathon </p>
+                            <p class="px-3" style="font-size: 10pt;">
+                                “Selama saya mengikuti KMIPN, banyak hal yang bisa saya dapatkan. Mulai dari pengalaman, relasi, dan kesempatan - kesempatan emas lainnya. Skill saya jugaberkembang setelah mengikuti lomba tersebut karena telah direview oleh juri yang profesional. “ - Nanang,2018.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                 <div class="col">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                           <img src="/image/reviewer/Muhammad zaid.jpg" alt="" class=" img-fluid rounded-circle p-3" style="width: 120px; height: 120px;display: block; margin-left: auto; margin-right: auto;">
+                            <h5 class="px-3 pt-3 text-bold">Mahdiya Aqila</h5>
+                            <p class="px-3 text-bold" style="font-size: 10pt; opacity: 50%;">KMIPN Hackathon </p>
+                            <p class="px-3" style="font-size: 10pt;">
+                                “Selama saya mengikuti KMIPN, banyak hal yang bisa saya dapatkan. Mulai dari pengalaman, relasi, dan kesempatan - kesempatan emas lainnya. Skill saya jugaberkembang setelah mengikuti lomba tersebut karena telah direview oleh juri yang profesional. “ - Nanang,2018.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                 <div class="col">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                           <img src="/image/reviewer/Muhammad zaid.jpg" alt="" class=" img-fluid rounded-circle p-3" style="width: 120px; height: 120px;display: block; margin-left: auto; margin-right: auto;">
+                            <h5 class="px-3 pt-3 text-bold">Mahdiya Aqila</h5>
+                            <p class="px-3 text-bold" style="font-size: 10pt; opacity: 50%;">KMIPN Hackathon </p>
+                            <p class="px-3" style="font-size: 10pt;">
+                                “Selama saya mengikuti KMIPN, banyak hal yang bisa saya dapatkan. Mulai dari pengalaman, relasi, dan kesempatan - kesempatan emas lainnya. Skill saya jugaberkembang setelah mengikuti lomba tersebut karena telah direview oleh juri yang profesional. “ - Nanang,2018.
+                            </p>
+                        </div>
+                    </div>
+                </div>
         </div>
         <div class="p-5"></div>
 
