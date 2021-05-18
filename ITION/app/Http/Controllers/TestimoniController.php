110f4aa0505b3d516d9023a30badb04da4ec8a1a
muhammadfagi <83048520+muhammadfagi@users.noreply.github.com>
2021-05-18 20:48:13 +0700

    Update testimoni

--- ITION/app/Http/Controllers/TestimoniController.php
+++ ITION/app/Http/Controllers/TestimoniController.php
@@ -14,13 +14,14 @@ class TestimoniController extends Controller
         $count = 0;
         foreach($kategori as $kategori_testimoni){
             $count++;
-            $data[$count] = DB::select("SELECT reviewer.nama,reviewer.foto,lomba.judul,testimoni.tahun_lomba,testimoni.testimoni,kategori.nama_kategori 
-            FROM reviewer,testimoni,lomba,kategori 
-            WHERE testimoni.id_lomba = lomba.id_lomba 
-            AND testimoni.id_reviewer = reviewer.id_reviewer 
-            AND lomba.id_kategori = kategori.id_kategori 
-            AND kategori.nama_kategori = '$kategori_testimoni->nama_kategori'");
+            $data[$count] = DB::select("SELECT reviewer.nama,reviewer.foto,reviewer.angkatan,lomba.judul,testimoni.tahun_lomba,testimoni.testimoni,kategori.nama_kategori 
+            FROM testimoni 
+            INNER JOIN reviewer ON testimoni.id_reviewer = reviewer.id_reviewer 
+            INNER JOIN lomba ON testimoni.id_lomba = lomba.id_lomba 
+            INNER JOIN kategori ON lomba.id_kategori = kategori.id_kategori 
+            WHERE kategori.nama_kategori = '$kategori_testimoni->nama_kategori'");
         }
-        return view('testimoni.testimoni',['kategori' => $kategori,'data' => $data]);
+
+        return view('testimoni.testimoni',['kategori' => $kategori, 'data'=> $data]);
     }
 }
