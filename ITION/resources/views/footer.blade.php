457d7c217b2f983aa3f0d94150d7259cd7848a96
MahdiyaAqila <mahdiyaqila@gmail.com>
2021-06-06 11:00:33 +0800

    footer responsive+layout lomba

--- ITION/resources/views/footer.blade.php
+++ ITION/resources/views/footer.blade.php
@@ -11,12 +11,10 @@
                 
                 <div class="bg-white-footer" style="border-radius: 35px;">
                     <div class="row">
-                        <div class="col-md-8">
+                        <div class="col-lg-8 col-md-12 col-sm-12">
                             <section class="">
                             <p class="text-black fw-bold fs-5">Ingin berbagi informasi lomba? </p>
-                            <p class="text-black fs-6"> Hai, Kami membuka kolaborasi dengan pihak penyelenggara lomba untuk menyebarkan informasi
-                                lombanya. Tenang saja, layanan kolaborasi kami GRATIS !! <br> 
-                                Tanpa dipungut biaya :D <br>
+                            <p class="text-black fs-6"> Hai, Kami membuka kolaborasi dengan pihak penyelenggara lomba untuk menyebarkan informasilombanya. Tenang saja, layanan kolaborasi kami GRATIS !! Tanpa dipungut biaya :D <br/>
                             </p>
                             <p class="mt-4">
                                 Apakah kalian tertarik ?
@@ -25,15 +23,43 @@
                             <button class="button-footer fw-bold" onclick="window.open('https://wa.me/6287701437591?text=Halo kak, saya ingin melakukan kolaborasi')">Yuk kolaborasi</button>
                             </section>
                         </div>
-
-                        <div class="col-md-4">
-                            <img class="img-fluid" src="{{asset('image/footer.png')}}">
+                        <div class="col-lg-4 ">
+                            <img class="img-hide" src="{{asset('image/footer.png')}}">
+                        
                         </div>
                     </div>
                 </div>
 
+                <div class="newsletter-area">
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 text-area p-5 ">
+                            <h4> Dapatkan update lomba setiap minggu !! </h4>
+                          Kalian maniak lomba ? Ingin mendapatkan banyak prestasi ?
+                          Jangan lewatkan layanan newsletter kami untuk mendapatkan informasi lomba disetiap minggunya. Tunggu apa lagi daftaran email kamu segera !!
+                        </div>
+                         <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 form-area p-5">
+                            <form>
+                                <div class="form-group">
+                                    <label for="exampleInputNama">Nama </label>
+                                    <input type="nama" class="form-control" id="exampleInputNama" placeholder="Tuliskan namamu...">
+                                </div>
+                                <br/>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Email</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Tuliskan emailmu...">
+                                    <small id="emailHelp" class="form-text text-muted">Kami tidak akan menyebarkan data anda</small>
+                                </div>
+                                <br/>
+                              <button type="submit" class="btn btn-warning button-footer btn-newsletter">Dapatkan notifikasi</button>
+                            </form>
+                                                       
+                        </div>
+                    </div>
+                        
+                </div>
+
                 <div class="row">
-                    <div class="col-md-4 col-sm-6">
+                    <div class="col-lg-6 col-md-6 col-sm-6">
                         <p class="text-white fw-bold contact"> 
                             Contact Person Admin 
                         </p>
@@ -43,35 +69,29 @@
                         </p>
                     </div>
 
-                    <div class="col-md-4 col-sm-6 px-6"></div>
-
-                    <div class="col-md-4 col-sm-6 px-6">
-                        <div class="col-md-12">
-                            <div class="">
-                                <div class="contact text-center">
-                                    <ul>
-                                        <li class="round-btn btn-items"><a href="#">
-                                            <img class="" src="{{asset('logo/1200px-Instagram_logo_2016 1.png')}}">
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                        <div class="contact">
+                                    <ul class="ul-top">
+                                        <li class="round-btn"><a href="#">
+                                            <img class="btn-img" src="{{asset('logo/1200px-Instagram_logo_2016 1.png')}}">
                                         </a></li>
-                                        <li class="round-btn btn-items"><a href="mailto:infoITION@gmail.com">
-                                            <img src="{{asset('logo/logo-gmail 1.png')}}">
+                                        <li class="round-btn"><a href="mailto:infoITION@gmail.com">
+                                            <img class="btn-img" src="{{asset('logo/logo-gmail 1.png')}}">
                                         </a></li>
-                                        <li class="round-btn btn-items"><a href="https://wa.me/6287701437591">
-                                            <img src="{{asset('logo/WhatsApp-Logo 1.png')}}">
+                                        <li class="round-btn btn-wa"><a ref="https://wa.me/6287701437591">
+                                            <img class="btn-img" src="{{asset('logo/WhatsApp-Logo 1.png')}}">
                                         </a></li>
                                     </ul>   
                                                                         
-                                    <ul>
-                                        <li class="btn-items top-footer">
-                                            <img src="{{asset('logo/Logo_PENS putih.png')}}">
-                                            <img src="{{asset('logo/Putih.png')}}">
-                                        </li>
+                                    <ul class="ul-bottom">
+                                        <li class="btn-items-bottom">
+                                            <img class="white-logo-pens" src="{{asset('logo/Logo_PENS putih.png')}}"/>
+                                            <img class="white-logo-himit" src="{{asset('logo/Putih.png')}}"/> </li>
                                     </ul>
-                                </div>
                             </div>
                         </div>
-                    </div>
 				</div>
+
                 <p class="text-white text-center fw-bold">
                     Copyright ITION 2021
                 </p>
--- ITION/resources/views/lomba/lomba.blade.php
+++ ITION/resources/views/lomba/lomba.blade.php
@@ -1,5 +1,5 @@
 <head>
-    <link rel="stylesheet" type="text/css" {{ asset('css/style.css') }}>
+   <link rel="stylesheet" type="text/css" href="{{ asset('css/lomba.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Lomba</title>
 </head>
@@ -53,65 +53,51 @@
     </div>
 
     <div class="container">
-        
         <div class="p-5"></div>
         <div class="mt-1"></div>
-
         <h1 class="fw-bold text-center">Ikuti Lomba</h1>
-
         <div class="p-4"></div>
-
-        <div class="row">
-            @foreach ($data as $item)
-            <div class="col-md-4 col-sm-6">
-                <div class="p-3"></div>
-                
-                <div class="card" style="width: 23rem; border-radius:30px">
-                    <div class="row mx-auto">
-                        <img src="{{ url('image/lomba/'.$item->poster) }}" class="card-img" alt="" style="border-radius: 26px 26px 0px 0px; max-width: 370px; height: 370px;">
-                        
-                        <p class="card-text ps-3 text-grey pt-2">Publish </p>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-7 col-sm-7">
-                            <div class="card-body">
-                                <h5 class="card-text fw-bold text-blue font-16px">{{ $item->judul }}</h5>
-                                <h6 class="card-text fw-light text-blue font-14px">{{ $item->nama_kategori }}</h6>
-                                
-                                <p class="card-text text-grey mt-4 font-12px">
-                                {{ \Illuminate\Support\Str::limit($item->deskripsi, 50, $end='...') }}
-                                </p>
-                            </div>
+        <div class="content-section">
+        <div class="card-place">
+            <div class="row">
+                @foreach ($data as $item)
+                <div class="col-lg-6 col-md-12 col-sm-12 p-5 ">
+                    <div class="card" style="width: 20rem; height: 450px; border-radius:20px">
+                        <div class="">
+                            <img src="{{ url('image/lomba/'.$item->poster) }}" class="card-img" alt="" style="border-radius: 20px 20px 0px 0px; max-width: 370px; height: 300px;">
+                            <p class="card-text ps-3 text-grey pt-2">Publish </p>
                         </div>
-
-                        <div class="col-md-5 col-sm-5 mt-0 pt-0">
-                            <div class="p-2"></div>
-
-                            <div class="card-body bg-blue-2 text-center">
-                                <p class="card-text fw-bold text-blue text-white font-12px">DEADLINE <br>
-                                PENDAFTARAN</p>
-
-                                <div class="p-1"></div>
+                        <div class="card-body">
+                                    <h5 class="card-text fw-bold text-blue font-12px">{{ $item->judul }}</h5>
+                                    <h6 class="card-text fw-light text-blue font-12px">{{ $item->nama_kategori }}</h6>
+                                    
+                                    <p class="card-text text-grey mt-4 font-12px">
+                                    {{ \Illuminate\Support\Str::limit($item->deskripsi, 25, $end='...') }}
+                                    </p>
                                 
-                                <p class="card-text mt-4 text-white font-12px text-uppercase">
-                                {{ \Carbon\Carbon::parse($item->deadline)->format('d F Y') }}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                                 <div class="card-body bg-blue-2 text-center blue-box">
+                                        <p class="card-text fw-bold text-blue text-white font-12px">DEADLINE <br>
+                                        PENDAFTARAN</p>
 
-                    <div class="row ms-0" style="width: 23rem;">
-                        <button class="button-katalog fw-bolder text-blue-2" onclick="window.location='{{ url("lomba/$item->id_lomba") }}'">Daftar Lomba Ini</button>
-                    </div>
+                                        <div class="p-1"></div>
+                                        
+                                        <p class="card-text mt-4 text-white font-12px text-uppercase">
+                                        {{ \Carbon\Carbon::parse($item->deadline)->format('d F Y') }}
+                                        </p>
+                                </div>
+                                <button class="button-katalog fw-bolder text-blue-2 card-button" onclick="window.location='{{ url("lomba/$item->id_lomba") }}'">Daftar Lomba Ini</button>
+                        </div>
+                    </div>      
                 </div>
+                @endforeach
             </div>
-            @endforeach
+            </div>
+            <div class="filter-place">
+                <h1>filter disini yaa buat div baru</h1>
+            </div>
         </div>
-    </div>
-    
+</div>
     <div class="p-5"></div>
-
     @include('footer')
 </body>
 </html>
