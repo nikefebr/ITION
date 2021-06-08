844312f1260b4848577f1d2b24a84eec0aab00e2
MahdiyaAqila <mahdiyaqila@gmail.com>
2021-06-08 12:57:25 +0800

    tentang responsive

--- ITION/resources/views/tentang/tentang.blade.php
+++ ITION/resources/views/tentang/tentang.blade.php
@@ -1,5 +1,10 @@
 <head>
+<<<<<<< Updated upstream
     <link rel="stylesheet" type="text/css" href="{{ asset('css/tentang.css') }}">
+=======
+   <link rel="stylesheet" type="text/css" href="{{ asset('css/tentang.css') }}">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+>>>>>>> Stashed changes
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Tentang</title>
 </head>
@@ -8,87 +13,76 @@
     
     <div class="p-5"></div>
     
-    <div class="container p-5">
-        <div class="row">
-            <div class="col-md-4">
-                <img src="{{asset('image/tentang/ition.jpeg' ) }}" width="270" >
+    <div class="container">
+        <div class="row p-5">
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 ">
+                <img class="img-ition" src="{{asset('image/tentang/ition.png' ) }}">
             </div>
 
-            <div class="col-md-7">
+            <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12text-ition">
                 <p class="fw-bold fs-3 text-black">
                     ITION 
                 </p>
-                <p class="text-black">
-                    ITION atau IT Competition merupakan website yang dibuat sebagai project <br>
-                    mata kuliah Workshop Produksi Perangkat Lunak Tahun 2021. Website ini<br>
-                    bertujuan guna membantu mahasiswa khususnya Teknik Informatika <br>
-                    dalam mendapatkan informasi lomba di bidang IT. 
+                <p class="text-black text-deskripsi">
+                    ITION atau IT Competition merupakan website yang dibuat sebagai project mata kuliah Workshop Produksi Perangkat Lunak Tahun 2021. Website ini bertujuan guna membantu mahasiswa khususnya Teknik Informatika dalam mendapatkan informasi lomba di bidang IT. 
                 </p>
             </div>
             
             <div class="col-md-2"></div>
         </div>
-    </div>
     
     <div class="p-4"></div>
         <p class="fw-bold fs-3 text-black text-center">
             Nilai Kami
         </p>
     <div class="p-2"></div>
-
-    <div class="container">
-        <div class="row mx-auto ps-5">
-        <div class="row mx-auto ps-5">
-        
-            <div class="col-md-4 col-sm-4">
-                <div class="p-4"></div>  
-                <div class="ps-4">
-                    <img src= "{{asset('image/tentang/motivasi.png')}}" class=""  alt="" width="166" height="159">
+    
+    <div class="content-nilai">
+        <div class="row d-flex justify-content-center" data-aos="fade-up">
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <div class="ps-4 img" >
+                    <img src= "{{asset('image/tentang/motivasi.png')}}" class=""  alt="" width="130" height="130">
                 </div>
         
-                <div class="card shadow" style="background-color:#003567; width: 15rem; height: 16rem; border-radius:30px;">
+                <div class="card shadow margin-50" style="background-color:#003567; width: 15rem; height: 16rem; border-radius:30px;">
                     <div class="card-body px-4">
-                            <br> <p class="card-text fw-bold text-white">Memotivasi</p>
+                            <br/> <br/> <br/> <p class="card-text fw-bold text-white">Memotivasi</p>
                             <p class="card-text fw-normal text-white">Mahasiswa mendapat testimoni dari mahasiswa lain yang pernah mengikuti lomba.</p>
                             <br>
                         </div>
                     </div>
                 </div>
 
-            <div class="col-md-4 col-sm-4"> 
-                <div class="p-4"></div>  
-                
-                <div class="ps-4">
-                    <img src= "{{asset('image/tentang/akurat.png')}}" class=""  alt="" width="166" height="159">
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"> 
+                <div class="ps-4 img">
+                    <img src= "{{asset('image/tentang/akurat.png')}}" class=""  alt=""width="130" height="130">
                 </div>
                 
-                <div class="card shadow" style="background-color:#003567; width: 15rem; height: 16rem; border-radius:30px;">
+                <div class="card shadow margin-50 " style="background-color:#003567; width: 15rem; height: 16rem; border-radius:30px;">
                     <div class="card-body px-4">
-                        <br><p class="card-text fw-bold text-white">Akurat</p>
+                        <br/> <br/> <br/> <p class="card-text fw-bold text-white">Akurat</p>
                         <p class="card-text fw-normal text-white">Mahasiswa mendapatkan informasi lomba secara akurat.</p>
                         <br><br>
                     </div>
                 </div>
             </div>
 
-            <div class="col-md-4 col-sm-4">
-                <div class="p-4"></div>
-                
-                <div class="ps-4">
-                    <img src= "{{asset('image/tentang/integrasi.png')}}" class="" alt="" width="166" height="159">
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <div class="ps-4 img">
+                    <img src= "{{asset('image/tentang/integrasi.png')}}" class="" alt="" width="130" height="130">
                 </div>
                 
-                <div class="card shadow" style="background-color:#003567; width: 15rem; height: 16rem; border-radius:30px;">   
+                <div class="card shadow margin-50" style="background-color:#003567; width: 15rem; height: 16rem; border-radius:30px;">   
                     <div class="card-body px-4"> <br>    
-                        <p class="card-text fw-bold text-white">Integrasi</p>
+                          <br/> <br/> <p class="card-text fw-bold text-white">Integrasi</p>
                         <p class="card-text fw-normal text-white">Mahasiswa dapat mencari informasi lomba di dalam satu sistem informasi.</p>
                         <br><br>
                     </div>
                 </div>
             </div>
         </div>
-        </div>
     </div>
+        
 
     <div class="p-4"></div>
     
@@ -98,18 +92,19 @@
         Partner Kami
     </p>
     <br><br>
+    <div class="content-partner">
+        <div class="row text-center" data-aos="fade-up">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 p-2">
+                <img class="img-partner" src="{{asset('image/tentang/pens.png' ) }}" width="120" >
+            </div>
+            
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 p-2">
+                <img class="img-partner" src="{{asset('image/tentang/it.png' ) }}" width="120" >
+            </div>
+        </div>
 
-    <div class="row">
-        <div class="col-md-4"></div>
-        
-        <div class="col-md-3">
-            <img src="{{asset('image/tentang/pens.png' ) }}" width="120" ></div>
-        
-        <div class="col-md-4">
-            <img src="{{asset('image/tentang/it.png' ) }}" width="120" ></div>
-        <div class="col-md-1"> </div>
     </div>
-
+    
 
     <div class="p-4"></div>
 
@@ -117,16 +112,12 @@
         <p class="fw-bold fs-3 text-black text-center">
             Tim Kami
         </p>
-    </div>
 
-    <div class="container p-5" >
+    <div class="content-tim p-5" data-aos="fade-up" >
         <div class="bg-white-footer shadow" style="border-radius: 20px;  background-color:#FFFFFF; ">
-            <div class="row">
-                <div class="col-md-2 col-sm-12"></div>
-
-                <div class="col-md-3 col-sm-6 ">
+            <div class="row px-5">
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 ">
                     <div class="p-4"></div>
-                    
                     <img src="{{ asset('image/tentang/nike.png')}}" class="card-img-top" alt="" width="225" height="170">
                     
                     <div class="card" style="width: 12rem; border:none;">
@@ -139,7 +130,7 @@
                     </div>
                 </div>
                     
-                <div class="col-md-3 col-sm-8"> 
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 "> 
                     <div class="p-4"></div>  
                     
                     <img src="{{ asset('image/tentang/aqila.png')}}" class="card-img-top" alt="" width="230" height="170">
@@ -154,7 +145,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-3 col-sm-12">
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 ">
                     <div class="p-4"></div>
                     
                     <img src="{{ asset('image/tentang/arita.png')}}" class="card-img-top" alt="" width="230" height="170">
@@ -169,13 +160,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-2"></div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-2 col-sm-12" ></div>
-                
-                <div class="col-md-3 col-sm-6 ">
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 ">
                     <div class="p-4"></div>
                     
                     <img src="{{ asset('image/tentang/aldo.png')}}" class="card-img-top" alt="" width="250" height="150">
@@ -190,7 +175,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-3 col-sm-8"> 
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12  "> 
                     <div class="p-4"></div>  
                     
                     <img src="{{ asset('image/tentang/fagi.png')}}" class="card-img-top" alt="" width="250" height="150">
@@ -205,7 +190,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-3 col-sm-12">
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 ">
                     <div class="p-4"></div>
                     
                     <img src="{{ asset('image/tentang/rafif.png')}}" class="card-img-top" alt="" width="250" height="150">
@@ -219,14 +204,20 @@
                         </div>
                     </div>
                 </div>
-                
-                <div class="col-md-2"></div>
-                
             </div>
         </div>
         <div class="p-4"></div>
     </div>
+ </div>
 
     @include('footer')
+      <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+          <script>
+            AOS.init({
+                offset :150,
+                duration :500,
+                easing: 'ease'
+            });
+          </script>
 </body>
 </html>
