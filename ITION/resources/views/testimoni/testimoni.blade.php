7701169298d4d459a4621636945207353da096c1
MahdiyaAqila <mahdiyaqila@gmail.com>
2021-06-08 13:14:52 +0800

    testimoni responsive

--- ITION/resources/views/testimoni/testimoni.blade.php
+++ ITION/resources/views/testimoni/testimoni.blade.php
@@ -1,5 +1,6 @@
 <head>
-    <link rel="stylesheet" type="text/css" {{ asset('css/style.css') }}>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/tentang.css') }}">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Testimoni</title>
 </head>
@@ -10,16 +11,15 @@
     <div class="pt-3"></div>
     
     <div class="bg-testimoni px-5"></div>
-
     <div class="container px-0 ms-5">
         <div class="pt-5"></div>
         <div class="pt-5"></div>
 
         @foreach ($kategori as $kat)
-        <div class="row ms-5">
+        <div class="row ms-5" data-aos="fade-up">
             <h2 class="fw-bold mb-5">Gimana sih rasanya ikut {{$kat->nama_kategori}}?</h2>
             @foreach ($data[$loop->iteration] as $item)
-                <div class="col">
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-4">
                     <div class="card mb-3 shadow" style="border-radius:30px; width: 20rem; height: 27rem;">
                         <div class="card-body">
                         <img src="{{ url('image/reviewer/'.$item->foto) }}" alt="" class=" img-fluid rounded-circle p-3" style="width: 120px; height: 120px;display: block; margin-left: auto; margin-right: auto;">
@@ -39,5 +39,13 @@
     </div>
     <div class="pt-5"></div>
     @include('footer')
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+          <script>
+            AOS.init({
+                offset :150,
+                duration :500,
+                easing: 'ease'
+            });
+          </script>
 </body>
 </html>
