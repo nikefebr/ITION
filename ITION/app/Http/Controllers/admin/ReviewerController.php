37a293338442f5088dc671665f05d08cc4595f79
bladeclara42 <71927457+bladeclara42@users.noreply.github.com>
2021-06-13 22:02:06 +0900

    create admin reviewer

---
+++ ITION/app/Http/Controllers/admin/ReviewerController.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\reviewer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Storage;
+
+class ReviewerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $reviewers = reviewer::all(); //mengambil semua isi tabel
+        return view('admin.reviewer.reviewer_view', compact('reviewers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.reviewer.reviewer_input');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // membuat validasi dari reviewer
+        $validated = Validator::make($request->all(),[
+            'nama' => ['required','unique:reviewer','max:255'],
+            'angkatan' => ['required','max:4'],
+            'foto' => ['required','image'],
+        ]);
+        
+        // mengecek apabila terdapat error atau tidak
+        if ($validated->fails()) {
+            return redirect()->route('create reviewer')->withErrors($validated); // redirect kembali dengan pesan error
+        } else {
+            
+            // akan membuat data baru dengan reviewer
+
+            $namedefault = $request->foto->getClientOriginalName(); //nama foto
+
+            $id_reviewer = reviewer::create([
+                'nama' => request('nama'),
+                'angkatan' => request('angkatan'),
+                'foto' => $namedefault,
+            ])->id_reviewer;
+            
+            $finder_reviewer = reviewer::find($id_reviewer);
+            
+            $name = 'gambar'.$id_reviewer.'.'.$request->foto->getClientOriginalExtension(); //mengganti nama foto dengan gambar+id
+
+            if ($finder_reviewer){
+                $finder_reviewer->foto = $name;
+                $finder_reviewer->save();
+            }
+
+            
+            $filePath = $request->foto->storeAs('reviewer',$name, ['disk' => 'public_image']); //lokasi penyimpanan foto
+            $request->foto->storeAs('reviewer',$name,['disk' => 'public_image']); //menyimpan foto
+
+            // redirect
+            return redirect()->route('view reviewer')->with('success','Reviewer baru berhasil dibuat!');
+        }
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\reviewer  $reviewer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(reviewer $reviewer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\reviewer  $reviewer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(reviewer $reviewer)
+    {
+        return view('admin.reviewer.reviewer_edit', compact('reviewer'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\reviewer  $reviewer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, reviewer $reviewer)
+    {
+        
+        //validasi data
+        $validated = Validator::make($request->all(),[
+            'nama' => ['required','max:255'],
+            'angkatan' => ['required','max:4'],
+            'foto' => ['required','image'],
+        ]);
+
+        //throw exception error
+        if ($validated->fails()) {
+            return redirect()->route('create reviewer')->withErrors($validated);
+        }
+        else {
+            $id_reviewer = $reviewer->id_reviewer;
+            $name = 'gambar'.$id_reviewer.'.'.$request->foto->getClientOriginalExtension(); //mengganti nama foto dengan gambar+id
+
+            //menghapus file yang ganda
+            $ext_img_before = pathinfo($reviewer->foto, PATHINFO_EXTENSION);
+            $ext_img_after = $request->foto->getClientOriginalExtension();
+
+            if ($ext_img_before != $ext_img_after) {
+                Storage::disk('public_image')->delete('reviewer/'.$reviewer->foto);
+            }
+            //end of delete
+
+            $reviewer->update([
+                'nama' => request('nama'),
+                'angkatan' => request('angkatan'),
+                'foto' => $name,
+            ]);
+
+            $filePath = $request->foto->storeAs('reviewer',$name, ['disk' => 'public_image']); //lokasi penyimpanan foto
+            $request->foto->storeAs('reviewer',$name,['disk' => 'public_image']); //menyimpan foto
+
+            return redirect()->route('view reviewer')->with('success','Reviewer '.$reviewer->nama.' berhasil diupdate!');
+        }   
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\reviewer  $reviewer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(reviewer $reviewer)
+    {
+        Storage::disk('public_image')->delete('reviewer/'.$reviewer->foto); //menghapus gambar
+
+        $request->lomba()->detach(); //menghapus semua testimoni yang terhubung drngan reviewer
+        $reviewer->delete(); //menghapus data dari reviewer tersebut
+
+        return redirect()->route('view reviewer')->with('success', 'Reviewer '.$reviewer->nama.' Berhasil Dihapus!');
+    }
+}
---
+++ ITION/resources/views/admin/reviewer/reviewer_view.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+    
+  <!-- add header -->
+  @include('admin.layout.header')
+
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <!-- add navbar display -->
+  @include('admin.layout.navbar')
+  
+  <!-- add sidebar -->
+  @include('admin.layout.sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">View data lomba</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+    
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                          @if ($message = Session::get('success'))
+                          <div class="card-header">
+                            <p class="alert alert-success card-text">{{ $message }}</p>
+                          </div>
+                          @endif
+                        <div class="card-header">
+                          <h3 class="card-title">DataTable with default features</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                          <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                            <tr>
+                              <th>No</th>
+                              <th width="200px">Nama Penyelenggara</th>
+                              <th>Kontak</th>
+                              <th>Nama Kontak</th>
+                              <th width="110px">Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @php
+                            $i = 0;
+                            @endphp 
+                            @foreach ($reviewers as $reviewer)
+                            <tr>
+                              <td>{{ ++$i }}</td>
+                              <td>
+                                <img src="{{ asset('image/reviewer/'.$reviewer->foto) }}" alt="{{ $reviewer->nama }}" class="img-thumbnail mx-auto d-block" style="max-width: 150px; max-height: 150px;"> 
+                              </td>
+                              <td>{{ $reviewer->nama }}</td>
+                              <td>{{ $reviewer->angkatan }}</td>
+                              <td>
+                                <form method="post" action="{{ route('destroy reviewer', $reviewer->id_reviewer) }}" >
+                                <div class="btn-group">
+                                  <a class="btn btn-warning" href="{{ route('edit reviewer', $reviewer->id_reviewer) }}">Edit</button></a>
+                                  @csrf
+                                  @method('DELETE')
+                                  <button type="submit"class="btn btn-danger" onclick="confirm('Reviewer {{ $reviewer->nama }} akan dihapus, lanjutkan?')">Delete</button>
+                                </div>
+                              </form>
+                              </td>
+                            </tr>
+                            @endforeach
+                            </tbody>
+                          </table>
+                        </div>
+                        <!-- /.card-body -->
+                      </div>
+                      <!-- /.card -->
+                </div>
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+      </div>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- add footer -->
+  @include('admin.layout.footer')
+
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+
+ <script src="{{ asset('js/app.js') }}"></script>
+<!-- 
+     jQuery 
+<script src="plugins/jquery/jquery.min.js"></script>
+     Bootstrap 4 
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+     AdminLTE App 
+<script src="dist/js/adminlte.min.js"></script>
+-->
+
+<!--page script-->
+<script>
+    
+  </script>
+
+</body>
+</html>
