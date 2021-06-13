bf9ef087e9ebd11ea4c327917c76f17d0eb72540
Aldo Faiz Winarno <71934691+aldofaiz@users.noreply.github.com>
2021-06-13 17:09:54 +0700

    update galeri (code & js)

--- ITION/resources/views/galeri/galeri.blade.php
+++ ITION/resources/views/galeri/galeri.blade.php
@@ -13,23 +13,22 @@
         <h1 class="fw-bold text-center">Galeri</h1>
 
         <div class="p-5"></div>
-        
+        <?php $index=0?>
         @foreach ($kategori as $kat)
         <div class="row">
             <h1 class="fw-bold">{{$kat->nama_kategori}}</h1>
         </div>
         
         <div class="row">
-            <div class="row">
-            
+            <div class="row">            
                 <div class="p-3"></div>
                 @foreach ($data[$loop->iteration] as $item)                
                 <div class="col-md-4 col-sm-6 float-start">
                     <div class="card" style="background-color:#F4F4F4; width: 18rem; border:none;">
-                        <img src="{{ $item->foto }}" class="card-img-top galeri-item" alt="" width="200" height="200" data-toggle="modal" data-target="#galeri-popup">
+                        <img src="{{ $item->foto }}" id="<?php echo $index++ ?>" class="card-img-top galeri-item" alt="" width="200" height="200" data-toggle="modal" data-target="#galeri-popup">
                             <div class="card-body">
-                                <p class="card-text fw-normal">{{ \Carbon\Carbon::parse($item->tgl_foto)->format('d F Y') }}</p>
-                                <p class="card-text fw-bold">{{ $item->deskripsi }}</p>
+                                <p class="card-text fw-normal tgl-foto">{{ \Carbon\Carbon::parse($item->tgl_foto)->format('d F Y') }}</p>
+                                <p class="card-text fw-bold deskripsi-foto">{{ $item->deskripsi }}</p>
 
                                 <div class="p-4"></div>
                             </div>     
@@ -51,12 +50,11 @@
                 
                     <img src="" class="card-img-top modal-img" alt="">
 
-                    <p class="font-14px pt-3 mb-0">Publish</p>
-                    <h5 class="fw-bold mt-0">Judul</h5>
+                    <p class="font-14px pt-3 mb-0" id="modal-tanggal"></p>
+                    <h5 class="fw-bold mt-0" id="modal-deskripsi"></h5>
 
                     <div class="pb-2"></div>                                        
-                </div>
-                
+                </div>                
             </div>
             </div>
         </div>
@@ -69,8 +67,12 @@
             if(e.target.classList.contains("galeri-item")){
                 const src = e.target.getAttribute("src");
                 document.querySelector(".modal-img").src = src;
+                const index = e.target.getAttribute("id");
+                const tgl = document.getElementsByClassName("tgl-foto")[index];
+                document.getElementById("modal-tanggal").innerHTML = tgl.innerHTML;
+                const deskripsi = document.getElementsByClassName("deskripsi-foto")[index];
+                document.getElementById("modal-deskripsi").innerHTML = deskripsi.innerHTML;
                 const myModal = new bootstrap.Modal(document.getElementById('galeri-popup'));
-                
             }
             myModal.show();
         })
